21001cb3bdfa52f3717bb075aab33b6c0cb56c42
inekruz <nekruz15ik@gmail.com>
2024-12-11 12:25:55 +0500

    Add code

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dbConfig: {
+      user: 'your_user',      
+      host: 'localhost',    
+      database: 'your_db',    
+      password: 'your_password',  
+      port: 5432,          
+    }
+  };
+  
--- index.js
+++ index.js
@@ -2,32 +2,57 @@ const express = require('express');
 const fs = require('fs');
 const https = require('https');
 const http = require('http');
+const { Client } = require('pg');  
+const config = require('./config'); 
 
-// Инициализация приложения
 const app = express();
-const portHttp = 80;  // Порт для HTTP
-const portHttps = 443;  // Порт для HTTPS
+const portHttp = 80;  // HTTP
+const portHttps = 443;  // HTTPS
 
-// Настройка маршрута
+const client = new Client(config.dbConfig);
+
+client.connect()
+  .then(() => console.log('Подключение к базе данных успешно!'))
+  .catch(err => console.error('Ошибка подключения к базе данных:', err));
+
 app.get('/', (req, res) => {
-  res.send('Hello from API.Glimshop.ru!');
+  res.send('Приветствую, сервер работает в штатном режиме!');
 });
 
-// Настройка HTTP для перенаправления на HTTPS
+app.get('/data', async (req, res) => {
+  try {
+    const result = await client.query('SELECT * FROM table');
+    res.json(result.rows);  
+  } catch (error) {
+    console.error('Ошибка при запросе к базе данных:', error);
+    res.status(500).json({ error: 'Ошибка при запросе к базе данных' });
+  }
+});
+
+app.post('/data', express.json(), async (req, res) => {
+  const { name, age } = req.body; 
+  try {
+    const result = await client.query('INSERT INTO table (name, age) VALUES ($1, $2) RETURNING *', [name, age]);
+    res.status(201).json(result.rows[0]);  
+  } catch (error) {
+    console.error('Ошибка при добавлении данных в базу:', error);
+    res.status(500).json({ error: 'Ошибка при добавлении данных в базу' });
+  }
+});
+
 http.createServer((req, res) => {
   res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
   res.end();
 }).listen(portHttp, () => {
-  console.log(`HTTP server running on port ${portHttp}`);
+  console.log(`HTTP запущен на порту - ${portHttp}`);
 });
 
-// Настройка HTTPS сервера
 const sslOptions = {
-  key: fs.readFileSync('/etc/letsencrypt/live/api.glimshop.ru/privkey.pem'),  // Путь к вашему приватному ключу
-  cert: fs.readFileSync('/etc/letsencrypt/live/api.glimshop.ru/cert.pem'),  // Путь к вашему сертификату
-  ca: fs.readFileSync('/etc/letsencrypt/live/api.glimshop.ru/fullchain.pem')  // Путь к вашему цепочечному сертификату
+  key: fs.readFileSync('/etc/letsencrypt/live/api.glimshop.ru/privkey.pem'), 
+  cert: fs.readFileSync('/etc/letsencrypt/live/api.glimshop.ru/cert.pem'), 
+  ca: fs.readFileSync('/etc/letsencrypt/live/api.glimshop.ru/fullchain.pem')
 };
 
 https.createServer(sslOptions, app).listen(portHttps, () => {
-  console.log(`HTTPS server running on port ${portHttps}`);
+  console.log(`HTTPS запущен на порту - ${portHttps}`);
 });
