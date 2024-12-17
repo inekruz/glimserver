bfcef08e89c4c9f7fae728dc4945f6501c5e3086
inekruz <nekruz15ik@gmail.com>
2024-12-17 12:47:33 +0500

    add profile data

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ app.post('/login', async (req, res) => {
 // Получение данных пользователя по логину
 app.post('/getUser', async (req, res) => {
   const { login } = req.body;
-  console.log("login: ", login[0]);
+  console.log("logggggiiin: ", login);
   if (!login[0]) {
     return res.status(400).json({ error: 'Логин не указан!' });
   }
